ec7c0ee1148a881e264893d7103e6d23b28ba1a3
Russentt <142530316+Russentt@users.noreply.github.com>
2025-11-20 22:47:13 -0300

    Update safepass.py

--- safepass.py
+++ safepass.py
@@ -3,16 +3,21 @@ import string
 import pyperclip
 
 def random_string(length):
-    characters = string.ascii_letters + string.digits + string.punctuation
+    if length >= 8:
+        characters = string.ascii_letters + string.digits + string.punctuation
+        random_characters = [secrets.choice(characters) for _ in range(length)]
+        return ''.join(random_characters)
+    else:
+        raise ValueError("Length is too short")
 
-    random_characters = [secrets.choice(characters) for _ in range(length)]
-
-    return ''.join(random_characters)
 
 
-
+print("This is a password generator. You must send a length equal or greather than 8")
 length_asked_to_user = int(input("Choose the length for your password: "))
 
-safe_password = random_string(length_asked_to_user)
-print(f'Your password is now copied to clipboard')
-pyperclip.copy(safe_password)
+try:
+    safe_password = random_string(length_asked_to_user)
+    pyperclip.copy(safe_password)
+    print("Your password is copied to clipboard")
+except ValueError as e:
+    print(f"Error: {e}")
