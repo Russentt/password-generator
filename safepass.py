46bf52aeeeafb76ad6085d26806abc4af69caae0
Russentt <142530316+Russentt@users.noreply.github.com>
2025-11-28 20:29:31 -0300

    Update safepass.py

--- safepass.py
+++ safepass.py
@@ -1,3 +1,6 @@
+import sys
+import os
+import stat
 import secrets
 import string
 import pyperclip
@@ -11,13 +14,19 @@ def random_string(length):
         raise ValueError("Length is too short")
 
 
+if os.geteuid() != 0:
+    print("Please, run this script using sudo")
 
-print("This is a password generator. You must send a length equal or greather than 8")
-length_asked_to_user = int(input("Choose the length for your password: "))
+else:
+    print("This is a password generator. You must send a length equal or greather than 8")
+    length_asked_to_user = int(input("Choose the length for your password: "))
+    try:
+        safe_password = random_string(length_asked_to_user)
+        pyperclip.copy(safe_password)
+        with open('pass.txt', 'a') as f:
+            f.write(f'{safe_password}\n')
+            os.chmod('pass.txt', stat.S_IRUSR | stat.S_IWUSR)
 
-try:
-    safe_password = random_string(length_asked_to_user)
-    pyperclip.copy(safe_password)
-    print("Your password is copied to clipboard")
-except ValueError as e:
-    print(f"Error: {e}")
+        print("Your password is copied to clipboard")
+    except ValueError as e:
+        print(f"Error: {e}")
